ddbf761f331e909560cd69a03f22671dce3cb430
MeIX968 <trantovietchi@gmail.com>
2026-05-06 13:14:40 +0700

    Temp

--- demo/Player.h
+++ demo/Player.h
@@ -49,7 +49,7 @@ namespace Demo {
 		DX9GF::ColliderManager* colliderManager = nullptr;
 		DX9GF::Camera* camera = nullptr;
 
-		int gold = 100;
+		int gold = 1000;
 		std::vector<std::string> deck;
 
 		std::vector<std::string> inventoryCards;
